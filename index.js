ad56eee6baee396581726c1afb4a0e67b461f0eb
saml <skynare@gmail.com>
2014-01-30 16:47:49 -0500

    display html code

--- index.js
+++ index.js
@@ -10,15 +10,20 @@ htmleditor.index.start = function(editorId, htmlId) {
     function onResize(evt) {
         var size = viewportMonitor.getSize();
         size.width = undefined;
-        size.height -= 10;
+        size.height -= 20;
         goog.style.setSize(goog.dom.getElement(editorId), size);
         goog.style.setSize(goog.dom.getElement(htmlId), size);
+    }
+
+    function updateHtml(evt) {
+        goog.dom.getElement(htmlId).value = editor.getCleanContents();
     }
 
     var viewportMonitor = new goog.dom.ViewportSizeMonitor();
     goog.events.listen(viewportMonitor, goog.events.EventType.RESIZE, onResize);
     
     var editor = new goog.editor.Field(editorId);
+    goog.events.listen(editor, goog.editor.Field.EventType.DELAYEDCHANGE, updateHtml);
     editor.makeEditable();
     editor.focusAndPlaceCursorAtStart();
     onResize();
